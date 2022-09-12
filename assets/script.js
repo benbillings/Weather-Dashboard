16505f0ae8bf69b0bbff090774223635eb14a45e
BenBill <ben.billings.02@icloud.com>
2022-09-12 19:05:12 -0400

    added bootstrap styling

--- assets/script.js
+++ assets/script.js
@@ -11,24 +11,32 @@ var day6DivEl = document.getElementById('day6');
 
 var apiKey = '520320703f34c13be437af71fe35996e';
 
-var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
-var apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+// var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+// var apiUrl2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
 
-function getCity(event) {
-    event.preventDefault();
+
+function getCity (event) {
     
+    event.preventDefault();
     var cityName = cityInputEl.value;
-
+    
     var cityHeading = document.createElement('h2');
     cityHeading.textContent = cityName;
-    cityHeading.id = 'cityHeading';
+    cityHeading.id = ('city-heading');
     currentWeatherDivEl.append(cityHeading);
 
-    // add most recent search to the history section
-    var lastSearch = document.createElement('p');
-    lastSearch.textContent = cityName;
-    searchHistoryDivEl.append('lastSearch');
-    // I need to make this clickable 
+    currentSearch = document.createElement('button');
+    currentSearch.textContent = cityName;
+    currentSearch.id = cityName;
+    currentSearch.id = 'result';
+    searchHistoryDivEl.append(currentSearch);
+    // currentSearch.setAttribute("href", "google.com")
+    
+    var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => getWeatherData(data))
+
+        searchInputEl.value = ""
 }
-
-
