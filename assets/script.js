5a25b2fde7b3726e3c6a87035bf90e6e2fbc2fa6
BenBill <ben.billings.02@icloud.com>
2022-09-15 19:07:43 -0400

    added dynamic html elemenets

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var cityInputEl = document.getElementById('cityInput');
-var searchBtnEl = document.getElementById('searchBtn');
+var searchBtnEl = document.getElementById('search-btn');
 var searchHistoryDivEl = document.getElementById('searchHistory');
 var currentWeatherDivEl = document.getElementById('currentWeather');
 var futureWeatherDivEl = document.getElementById('futureWeather');
@@ -11,16 +11,17 @@ var day6DivEl = document.getElementById('day6');
 
 const apiKey = '060d14e161e683217f175e18c35d2138';
 
-searchBtnEl.addEventListener('click', getCity);
+const cityArr = [];
 
 function getCity() {
-    var searchInput = cityInputEl.value;
-    localStorage.setItem('cityName', searchInput);
+    var id = document.getElementsByTagName('input')[0];
+    cityArr.push(id.value);
+    localStorage.setItem('cityLog', JSON.stringify(cityArr));
+    alert(localStorage.getItem('cityLog'));
 }
 
-// get city name
-var cityName = localStorage.getItem('cityName')
-fetchCity();
+
+// fetchCity();
 
 // function to convert city name into lat and lon
 function fetchCity(data) {
@@ -45,69 +46,72 @@ function fetchWeatherData(data) {
 function renderWeather(data) {
     console.log(data);
     // convert date from unix
-    let unixTimeStamp = data.dt;
-    let milliseconds = unixTimeStamp * 1000;
-    let dateObject = new Date(milliseconds)
-    let dateArr = dateObject.toLocaleString().split(',');
+    var unixTimeStamp = data.dt;
+    var milliseconds = unixTimeStamp * 1000;
+    var dateObject = new Date(milliseconds)
+    var dateArr = dateObject.toLocaleString().split(',');
     
-    let date = dateArr[0];
-    let currentTemp = data.main.temp;
-    let highTemp = data.main.temp_max;
-    let lowTemp = data.main.temp_min;
-    let currentHumidity = data.main.humidity;
-    let windSpeed = data.wind.speed;
-    let iconCode = data.weather[0].icon;
+    var date = dateArr[0];
+    var currentTemp = data.main.temp;
+    var highTemp = data.main.temp_max;
+    var lowTemp = data.main.temp_min;
+    var currentHumidity = data.main.humidity;
+    var windSpeed = data.wind.speed;
+    var iconCode = data.weather[0].icon;
     
-
     const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`
-    var cardDivEl = document.querySelector('.card');
     
-    let weatherIcon = document.createElement('img');
-    weatherIcon.src = iconUrl;
-    weatherIcon.alt = 'Icon displaying the current weather';
-    weatherIcon.className = 'card-img-top';
-    cardDivEl.appendChild(weatherIcon); 
-    weatherIcon.style.width = '50%';
-    weatherIcon.style.alignSelf = 'center';
+    
+    
+    
+//     var cardDivEl = document.querySelector('.card');
+    
+//     let weatherIcon = document.createElement('img');
+//     weatherIcon.src = iconUrl;
+//     weatherIcon.alt = 'Icon displaying the current weather';
+//     weatherIcon.className = 'card-img-top';
+//     cardDivEl.appendChild(weatherIcon); 
+//     weatherIcon.style.width = '50%';
+//     weatherIcon.style.alignSelf = 'center';
 
-    let cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
-    cardDivEl.appendChild(cardBody);
+//     let cardBody = document.createElement('div');
+//     cardBody.className = 'card-body';
+//     cardDivEl.appendChild(cardBody);
     
-    let renderCityName = document.createElement('h3');
-    renderCityName.textContent = cityName;
-    cardBody.appendChild(renderCityName);
-    renderCityName.className = 'card-title';
+//     let renderCityName = document.createElement('h3');
+//     renderCityName.textContent = cityName;
+//     cardBody.appendChild(renderCityName);
+//     renderCityName.className = 'card-title';
 
-    let renderDate = document.createElement('h4');
-    renderDate.textContent = date;
-    cardBody.appendChild(renderDate);
-    renderDate.className = 'card-title';
+//     let renderDate = document.createElement('h4');
+//     renderDate.textContent = date;
+//     cardBody.appendChild(renderDate);
+//     renderDate.className = 'card-title';
     
-    let renderTemp = document.createElement('p');
-    renderTemp.innerHTML = 'Current Temperature: ' + currentTemp + ' degrees';
-    cardBody.appendChild(renderTemp);
-    renderTemp.className = 'card-text';
+//     let renderTemp = document.createElement('p');
+//     renderTemp.innerHTML = 'Current Temperature: ' + currentTemp + ' degrees';
+//     cardBody.appendChild(renderTemp);
+//     renderTemp.className = 'card-text';
     
-    let renderHighTemp = document.createElement('p');
-    renderHighTemp.innerHTML = 'High Temperature: ' + highTemp + ' degrees';
-    cardBody.appendChild(renderHighTemp);
-    renderHighTemp.className = 'card-text';
+//     let renderHighTemp = document.createElement('p');
+//     renderHighTemp.innerHTML = 'High Temperature: ' + highTemp + ' degrees';
+//     cardBody.appendChild(renderHighTemp);
+//     renderHighTemp.className = 'card-text';
     
-    let renderLowTemp = document.createElement('p');
-    renderLowTemp.innerHTML = 'Low Temperature: ' + lowTemp + ' degrees';
-    cardBody.appendChild(renderLowTemp);
-    renderLowTemp.className = 'card-text';
+//     let renderLowTemp = document.createElement('p');
+//     renderLowTemp.innerHTML = 'Low Temperature: ' + lowTemp + ' degrees';
+//     cardBody.appendChild(renderLowTemp);
+//     renderLowTemp.className = 'card-text';
     
-    let renderHumidity = document.createElement('p');
-    renderHumidity.innerHTML = 'Humidity: ' + currentHumidity + '%';
-    cardBody.appendChild(renderHumidity);
-    renderHumidity.className = 'card-text';
+//     let renderHumidity = document.createElement('p');
+//     renderHumidity.innerHTML = 'Humidity: ' + currentHumidity + '%';
+//     cardBody.appendChild(renderHumidity);
+//     renderHumidity.className = 'card-text';
     
-    let renderWindSpeed = document.createElement('p');
-    renderWindSpeed.innerHTML = 'Wind Speed: ' + windSpeed + ' mph';
-    cardBody.appendChild(renderWindSpeed);
-    renderWindSpeed.className = 'card-text';
+//     let renderWindSpeed = document.createElement('p');
+//     renderWindSpeed.innerHTML = 'Wind Speed: ' + windSpeed + ' mph';
+//     cardBody.appendChild(renderWindSpeed);
+//     renderWindSpeed.className = 'card-text';
 }
 
 
