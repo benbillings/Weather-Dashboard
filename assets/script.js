a839535da6da5ad2ea850b3258e5164614d4b0e5
BenBill <ben.billings.02@icloud.com>
2022-09-16 12:43:54 -0400

    rewrite local storage

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var cityInputEl = document.getElementById('cityInput');
 var searchBtnEl = document.getElementById('search-btn');
-var searchHistoryDivEl = document.getElementById('searchHistory');
-var currentWeatherDivEl = document.getElementById('currentWeather');
-var futureWeatherDivEl = document.getElementById('futureWeather');
+var searchHistoryDivEl = document.getElementById('search-history');
+var currentWeatherDivEl = document.getElementById('current-weather');
+var futureWeatherDivEl = document.getElementById('future-weather');
 var day2DivEl = document.getElementById('day2');
 var day3DivEl = document.getElementById('day3');
 var day4DivEl = document.getElementById('day4');
@@ -11,120 +11,137 @@ var day6DivEl = document.getElementById('day6');
 
 const apiKey = '060d14e161e683217f175e18c35d2138';
 
-const cityArr = ['hello', 'goodbye', 'japan'];
 
-function getCity() {
-    var id = document.getElementsByTagName('input')[0];
-    cityArr.push(id.value);
-    id.value = '';
-    console.log(cityArr);
-    localStorage.setItem('cityLog', JSON.stringify(cityArr));
+
+
+
+searchBtnEl.addEventListener('click', function() {
+    recordSearchHistory();
+})
+
+
+var searchHistoryArr = JSON.parse(localStorage.getItem('city-log'));
+if (!searchHistoryArr) {
+    searchHistoryArr = [];
+}
+
+function recordSearchHistory() {
+    var cityName = cityInputEl.value;
+    searchHistoryArr.push(cityName);
+    localStorage.setItem('city-log', JSON.stringify(searchHistoryArr));
+
+    if (searchHistoryArr) {
+        for (var i = 0; i < searchHistoryArr.length; i++) {
+            console.log(searchHistoryArr[i]);
+        }
     
-    alert(localStorage.getItem('cityLog'));
+    
+    // var recentSearchesHeader = document.createElement('h2');
+    
+    var listSearchHistory = document.createElement('ul')
+    listSearchHistory.setAttribute('class', 'list-group list-group-flush');
+
+    var mostRecentSearch = document.createElement('li');
+    mostRecentSearch.setAttribute('class', 'list-group-item');
+    mostRecentSearch.textContent = searchHistoryArr[0];
+    
+    searchHistoryDivEl.append(listSearchHistory, mostRecentSearch);
+    listSearchHistory.prepend(mostRecentSearch);
 
 
 }
 
-console.log()
 
 
-// fetchCity();
+
+fetchCity();
 
 // function to convert city name into lat and lon
-// function fetchCity(data) {
-//     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
-//     .then(response => response.json())
-//     .then(data => fetchWeatherData(data));
-// }
+function fetchCity(data) {
+    var cityName = cityInputEl.value;
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => function (data){
+    console.log(data);
+    
+    var latitude = data[0].lat;
+    var longitude = data[0].lon;
+    console.log(latitude);
+    console.log(longitude);
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => renderWeather(data))})
+}    
 
-// function fetchWeatherData(data) {
-//     console.log(data);
+function renderWeather(data) {
+    console.log(data);
+    // convert date from unix
+    var unixTimeStamp = data.dt;
+    var milliseconds = unixTimeStamp * 1000;
+    var dateObject = new Date(milliseconds)
+    var dateArr = dateObject.toLocaleString().split(',');
     
-//     var latitude = data[0].lat;
-//     var longitude = data[0].lon;
-//     console.log(latitude);
-//     console.log(longitude);
+    var date = dateArr[0];
+    var currentTemp = data.main.temp;
+    var highTemp = data.main.temp_max;
+    var lowTemp = data.main.temp_min;
+    var currentHumidity = data.main.humidity;
+    var windSpeed = data.wind.speed;
+    var iconCode = data.weather[0].icon;
     
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`)
-//     .then(response => response.json())
-//     .then(data => renderWeather(data)) 
-// }    
+    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`
+    
+    
+    
+    
+    var cardDivEl = document.querySelector('.card');
+    
+    let weatherIcon = document.createElement('img');
+    weatherIcon.src = iconUrl;
+    weatherIcon.alt = 'Icon displaying the current weather';
+    weatherIcon.className = 'card-img-top';
+    cardDivEl.appendChild(weatherIcon); 
+    weatherIcon.style.width = '50%';
+    weatherIcon.style.alignSelf = 'center';
 
-// function renderWeather(data) {
-//     console.log(data);
-//     // convert date from unix
-//     var unixTimeStamp = data.dt;
-//     var milliseconds = unixTimeStamp * 1000;
-//     var dateObject = new Date(milliseconds)
-//     var dateArr = dateObject.toLocaleString().split(',');
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    cardDivEl.appendChild(cardBody);
     
-//     var date = dateArr[0];
-//     var currentTemp = data.main.temp;
-//     var highTemp = data.main.temp_max;
-//     var lowTemp = data.main.temp_min;
-//     var currentHumidity = data.main.humidity;
-//     var windSpeed = data.wind.speed;
-//     var iconCode = data.weather[0].icon;
-    
-    // const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`
-    
-    
-    
-    
-//     var cardDivEl = document.querySelector('.card');
-    
-//     let weatherIcon = document.createElement('img');
-//     weatherIcon.src = iconUrl;
-//     weatherIcon.alt = 'Icon displaying the current weather';
-//     weatherIcon.className = 'card-img-top';
-//     cardDivEl.appendChild(weatherIcon); 
-//     weatherIcon.style.width = '50%';
-//     weatherIcon.style.alignSelf = 'center';
+    let renderCityName = document.createElement('h3');
+    renderCityName.textContent = cityName;
+    cardBody.appendChild(renderCityName);
+    renderCityName.className = 'card-title';
 
-//     let cardBody = document.createElement('div');
-//     cardBody.className = 'card-body';
-//     cardDivEl.appendChild(cardBody);
+    let renderDate = document.createElement('h4');
+    renderDate.textContent = date;
+    cardBody.appendChild(renderDate);
+    renderDate.className = 'card-title';
     
-//     let renderCityName = document.createElement('h3');
-//     renderCityName.textContent = cityName;
-//     cardBody.appendChild(renderCityName);
-//     renderCityName.className = 'card-title';
-
-//     let renderDate = document.createElement('h4');
-//     renderDate.textContent = date;
-//     cardBody.appendChild(renderDate);
-//     renderDate.className = 'card-title';
+    let renderTemp = document.createElement('p');
+    renderTemp.innerHTML = 'Current Temperature: ' + currentTemp + ' degrees';
+    cardBody.appendChild(renderTemp);
+    renderTemp.className = 'card-text';
     
-//     let renderTemp = document.createElement('p');
-//     renderTemp.innerHTML = 'Current Temperature: ' + currentTemp + ' degrees';
-//     cardBody.appendChild(renderTemp);
-//     renderTemp.className = 'card-text';
+    let renderHighTemp = document.createElement('p');
+    renderHighTemp.innerHTML = 'High Temperature: ' + highTemp + ' degrees';
+    cardBody.appendChild(renderHighTemp);
+    renderHighTemp.className = 'card-text';
     
-//     let renderHighTemp = document.createElement('p');
-//     renderHighTemp.innerHTML = 'High Temperature: ' + highTemp + ' degrees';
-//     cardBody.appendChild(renderHighTemp);
-//     renderHighTemp.className = 'card-text';
+    let renderLowTemp = document.createElement('p');
+    renderLowTemp.innerHTML = 'Low Temperature: ' + lowTemp + ' degrees';
+    cardBody.appendChild(renderLowTemp);
+    renderLowTemp.className = 'card-text';
     
-//     let renderLowTemp = document.createElement('p');
-//     renderLowTemp.innerHTML = 'Low Temperature: ' + lowTemp + ' degrees';
-//     cardBody.appendChild(renderLowTemp);
-//     renderLowTemp.className = 'card-text';
+    let renderHumidity = document.createElement('p');
+    renderHumidity.innerHTML = 'Humidity: ' + currentHumidity + '%';
+    cardBody.appendChild(renderHumidity);
+    renderHumidity.className = 'card-text';
     
-//     let renderHumidity = document.createElement('p');
-//     renderHumidity.innerHTML = 'Humidity: ' + currentHumidity + '%';
-//     cardBody.appendChild(renderHumidity);
-//     renderHumidity.className = 'card-text';
-    
-//     let renderWindSpeed = document.createElement('p');
-//     renderWindSpeed.innerHTML = 'Wind Speed: ' + windSpeed + ' mph';
-//     cardBody.appendChild(renderWindSpeed);
-//     renderWindSpeed.className = 'card-text';
-// }
-
-
-
-
-
-// let renderCity = document.createElement('button');
-// renderCity.textContent = cityName;
-// currentCityDisplay.appendChild(renderCity);
+    let renderWindSpeed = document.createElement('p');
+    renderWindSpeed.innerHTML = 'Wind Speed: ' + windSpeed + ' mph';
+    cardBody.appendChild(renderWindSpeed);
+    renderWindSpeed.className = 'card-text';
+    }
+}
